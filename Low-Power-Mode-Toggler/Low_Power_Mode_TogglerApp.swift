76c62196cc9048af11f2e4853056998b72cb209a
Andy Lin <roblox248@gmail.com>
2022-06-15 21:13:05 -0400

    added hookup to status bar

--- Low-Power-Mode-Toggler/Low_Power_Mode_TogglerApp.swift
+++ Low-Power-Mode-Toggler/Low_Power_Mode_TogglerApp.swift
@@ -12,10 +12,28 @@ import AppKit
 
 @main
 struct Low_Power_Mode_TogglerApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem!
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        let attribute = [NSAttributedString.Key.font: NSFont.systemFont(ofSize: 11)]
+        let internalFinder = InternalFinder();
+        if let internalBattery = internalFinder.getInternalBattery(){
+            let str = NSAttributedString(string: "\(Int(internalBattery.charge!))%", attributes: attribute)
+            statusItem.button?.attributedTitle = str
+        }else{
+            let str = NSAttributedString(string: "No Battery", attributes: attribute)
+            statusItem.button?.attributedTitle = str
         }
     }
 }
