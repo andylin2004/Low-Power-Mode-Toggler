2591b14cf7cb6fdb6c87844abd227f9700918af9
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-06-18 11:32:41 -0400

    starting to make the helper tool for xpc part

--- helper/main.swift
+++ helper/main.swift
@@ -6,5 +6,9 @@
 //
 
 import Foundation
+import SecureXPC
 
-
+if getppid() == 1 {
+    let server = try XPCServer.forThisBlessedHelperTool()
+    server.registerRoute(<#T##route: XPCRouteWithoutMessageWithoutReply##XPCRouteWithoutMessageWithoutReply#>, handler: <#T##() throws -> Void#>)
+}
