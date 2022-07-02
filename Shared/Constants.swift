bd62a6892ef4b6fa8c94d085c9d73ef4ac8f3d45
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-07-02 11:09:25 -0400

    added path to uninstall

--- Shared/Constants.swift
+++ Shared/Constants.swift
@@ -12,6 +12,8 @@ import Blessed
 struct Constants {
     static let changePowerMode = XPCRoute.named("changePowerMode")
         .withMessageType(LowPowerModeUpdate.self)
+    
+    static let uninstall = XPCRoute.named("uninstall")
 }
 
 struct LowPowerModeUpdate: Codable {
--- helper/main.swift
+++ helper/main.swift
@@ -8,9 +8,22 @@
 import Foundation
 import SecureXPC
 
-if getppid() == 1 {
+if CommandLine.arguments.count > 1 {
+    // Remove the first argument, which represents the name (typically the full path) of this helper tool
+    var arguments = CommandLine.arguments
+    _ = arguments.removeFirst()
+    
+    if let firstArgument = arguments.first {
+        if firstArgument == "uninstall" {
+            try Uninstaller.uninstallFromCommandLine(withArguments: arguments)
+        } else {
+            NSLog("argument not recognized: \(firstArgument)")
+        }
+    }
+} else if getppid() == 1 {
     let server = try XPCServer.forThisBlessedHelperTool()
     server.registerRoute(Constants.changePowerMode, handler: LowPowerController.changePowerMode(lowPowerUpdate:))
+    server.registerRoute(Constants.uninstall, handler: Uninstaller.uninstallFromXPC)
     server.setErrorHandler { error in
         if case .connectionInvalid = error {
             // this is when client disconnects, which is fine
